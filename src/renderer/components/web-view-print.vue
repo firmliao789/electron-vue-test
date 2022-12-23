10af8a980de2c74113242ef03e0fbb2ad25f6f54
廖毅 <firmliao789@126.com>
2022-12-23 10:11:58 +0800

    修改打印

--- src/renderer/components/web-view-print.vue
+++ src/renderer/components/web-view-print.vue
@@ -3,11 +3,11 @@
 <template>
   <div style="display:none;">
     <webview
-      ref="printWebview"
-      id="printWebview"
-      src="index.html#/print-view"
-      nodeintegration
-      style="visibility: hidden;"
+        ref="printWebview"
+        id="printWebview"
+        src="index.html#/print-view"
+        nodeintegration
+        style="visibility: hidden;"
     ></webview>
   </div>
 </template>
@@ -25,14 +25,14 @@ export default {
     addEventListener() {
       const webview = document.querySelector("#printWebview");
       webview.addEventListener("did-finish-load", msg =>
-        console.log("did-finish-load:", msg)
+          console.log("did-finish-load:", msg)
       );
       webview.addEventListener("load-commit", msg =>
-        console.log("load-commit:", msg)
+          console.log("load-commit:", msg)
       );
 
       webview.addEventListener("did-fail-load", msg =>
-        console.log("did-fail-load：", msg)
+          console.log("did-fail-load：", msg)
       );
 
       webview.addEventListener("ipc-message", async event => {
@@ -50,8 +50,8 @@ export default {
             this.updatePrintStatus(event.args[0].id);
             global.settingInfo.printNumber = global.settingInfo.printNumber - 1;
             window.localStorage.setItem(
-              "setting",
-              JSON.stringify(global.settingInfo)
+                "setting",
+                JSON.stringify(global.settingInfo)
             );
           } catch (err) {
             console.log("打印出错：", err);
@@ -66,9 +66,18 @@ export default {
       console.log("render:", this.prints);
       // if (this.prints.length > 0) {
       //   this.showLoading({ text: "系统正在出票..." });
-        const data = this.prints.shift();
-        console.log("发送渲染数据", data.id, +new Date());
-        this.$refs["printWebview"].send("webview-print-render", data);
+      const data = this.prints.shift();
+      console.log("发送渲染数据", data.id, +new Date());
+      setTimeout(async () => {
+        const ret = await webview.print({
+          silent: true,
+          printBackground: true
+          //deviceName: name
+        });
+        console.log("开始打印");
+      }, 5000)
+
+      // this.$refs["printWebview"].send("webview-print-render", data);
       // } else {
       //   this.hideLoading();
       // }
